f91085358a260964fa7895ef8efdab51f518aef1
Alejandro Soto Canales <vampirex.415@gmail.com>
2025-01-05 02:50:36 -0600

    crud Proveedores

--- src/pages/dashboard/suppliers.tsx
+++ src/pages/dashboard/suppliers.tsx
@@ -1,7 +1,52 @@
+import { Button, Card, TextInput } from "flowbite-react";
+import BarChart from "../../components/barChart";
+import TopPanel from "../../components/topPanel";
+import useDashboard from "../../hooks/useDashboard";
+import TableGenerator from "../../components/tableGenerator";
+import {
+  suppliersModal,
+  suppliersTableHeaders,
+  suppliersTableKeys,
+} from "../../assets/suppliersConfig";
+import EditModal from "../../components/editModal";
+
 export default function Suppliers() {
+  const { modalProps, tableProps, handleAdd } = useDashboard("proveedor");
+
   return (
-    <div className="">
-      <h1>Proveedores</h1>
-    </div>
+    <>
+      <div className="w-full h-full flex flex-col items-end gap-4">
+        {/* Stats ----------------------------------------------------------------------------------------- */}
+        <div className="grid grid-cols-2 w-full gap-4">
+          <TopPanel
+            title="Más vendidos"
+            series={[
+              { name: "material 1", description: "$3050 vendido", value: 50 },
+              { name: "material 2", description: "$3050 vendido", value: 50 },
+              { name: "material 3", description: "$3050 vendido", value: 50 },
+            ]}
+          />
+          <BarChart />
+        </div>
+
+        {/* Table ----------------------------------------------------------------------------------------- */}
+        <Card className="w-full border-gray-300 mt-5">
+          <div className="flex justify-end gap-28 w-full">
+            <TextInput placeholder="Buscar por nombre" className="grow" />
+            <Button gradientDuoTone="greenToBlue" onClick={handleAdd}>
+              Agregar nuevo
+            </Button>
+          </div>
+          <TableGenerator
+            {...tableProps}
+            headers={suppliersTableHeaders}
+            keys={suppliersTableKeys}
+          />
+        </Card>
+      </div>
+
+      {/* modal ----------------------------------------------------------------------------------------- */}
+      <EditModal {...modalProps} title="material" inputs={suppliersModal} />
+    </>
   );
 }
